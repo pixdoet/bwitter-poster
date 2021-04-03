ce63d0b6bb5bb3f9cb8b04bb835e2ada6d1bb8d6
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-04-03 15:10:23 +0800

    Added command feature and loop
    Now you can login as another user without restarting the program!

--- bwitter_poster.py
+++ bwitter_poster.py
@@ -11,13 +11,30 @@ class Bwitter():
         self.pword = input("Enter your password: ")
         self.credentials = {"username_or_email":self.uname,"password":self.pword, "remember_me":"1"}
         self.loginguy = self.s.post("https://bwitter.me/sessions", data=self.credentials)
-        print (self.loginguy.text)
+        #print (self.loginguy.text)
         #print (self.s.cookies.get_dict())
+    def post(self):
         self.context = input("What are you doing? Say it: ")
         self.topost = {"content":self.context}
         self.contextguy = self.s.post("https://bwitter.me/bweet", data = self.topost)
-        print (self.contextguy.text)
+        #print (self.contextguy.text)
+
+    def commands(self):
+        self.command = input("Type login to login, post to post(must login), x to exit: ")
     
 bwitter = Bwitter()
-bwitter.login()
-
+quit = False
+while not quit:
+    bwitter.commands()
+    try:
+        if bwitter.command == "login":
+            bwitter.login()
+        elif bwitter.command == "post":
+            if not bwitter.uname and not bwitter.pword:
+                bwitter.login()
+            else:
+                bwitter.post()
+        elif bwitter.command == "x":
+            quit = True
+    except:
+        print ("Command not found")
