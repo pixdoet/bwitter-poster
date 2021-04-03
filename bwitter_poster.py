0338ea539eacaeac96ff6848e42e5a7c695df479
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-04-03 12:17:13 +0800

    Added login system

--- bwitter_poster.py
+++ bwitter_poster.py
@@ -1,27 +1,25 @@
 # bwitter api thingy
 
+# bwitter api thingy
+
 import requests
 
 class Bwitter():
     def __init__(self):
-        self.logincookies = {}
-        self.session = requests.Session()
-        self.phpsess = ""
+        self.s = requests.Session()
 
     def login(self):
         self.uname = input("Enter your username: ")
         self.pword = input("Enter your password: ")
-        self.credentials = {"username_or_email":self.uname,"password":self.pword}
-        self.loginguy = self.session.post("https://bwitter.me/sessions/", data=self.credentials)
-        print (self.session.cookies.get_dict())
-        self.logincookies = self.session.cookies.get_dict()
-    
-    def post(self):
-        self.login2 = {"PHPSESSID":""}
+        self.credentials = {"username_or_email":self.uname,"password":self.pword, "remember_me":"1"}
+        self.loginguy = self.s.post("https://bwitter.me/sessions", data=self.credentials)
+        print (self.loginguy.text)
+        #print (self.s.cookies.get_dict())
         self.context = input("What are you doing? Say it: ")
         self.topost = {"content":self.context}
-        self.contextguy = requests.post("https://bwitter.me/bweet", data = self.topost, cookies = self.login2)
+        self.contextguy = self.s.post("https://bwitter.me/bweet", data = self.topost)
         print (self.contextguy.text)
     
 bwitter = Bwitter()
-bwitter.post()
+bwitter.login()
+
